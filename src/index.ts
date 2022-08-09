3e27b1af1da87239c1a37bdd8259b651a520c62d
kingrayhan <rayhan095@gmail.com>
2022-08-09 11:53:44 +0600

    |-_-|

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,11 @@ dotenv.config({
   path: ".env",
 });
 
-app.use(cors());
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
